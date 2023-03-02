dfa034cb9fad27611d90d70446a1477125fc8304
AudunTA <aud-and2@online.no>
2023-03-02 22:04:28 +0100

    added information section

--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -14,12 +14,38 @@ import {
 function About() {
   return (
     <div className="container-About">
-      <div className="container-portfolio">
+      <div className="wrapper-container">
         <div className="container-portfolio-work">
           <div className="top-bar">
             <div className="border-div-left"></div>
             <h2>About me</h2>
             <div className="border-div-about-right"></div>
+          </div>
+          <div className="information-section">
+            <div className="container-information">
+              <div className="information-header">
+                <div className="btn_content-about">
+                  <p>01. information</p>
+                </div>
+              </div>
+              <div className="flex-education">
+                <p className="triangle-icon">▹</p>
+                <p className="information-text-one">
+                  Hello! my name is Audun, im 25 years old and looking for a job
+                  in primarly Front End. I live in Kristiansand, but im not
+                  ruling out moving to start my career! I have strong
+                  fundamentals in programming, and currently working on getting
+                  better at React.
+                </p>
+              </div>
+              <div className="flex-education">
+                <p className="triangle-icon-margin-top">▹</p>
+                <p className="information-text-two">
+                  In my spare time I enjoy working out and hanging out with my
+                  friends & enjoy some nice food.
+                </p>
+              </div>
+            </div>
           </div>
           <div className="skills-section">
             <div className="about-header">
@@ -29,24 +55,71 @@ function About() {
             </div>
             <div className="icons-container">
               <div className="icon-divider">
-                <FontAwesomeIcon className="icon-brands" icon={faReact} />
-                <FontAwesomeIcon className="icon-brands" icon={faJs} />
-                <FontAwesomeIcon className="icon-brands" icon={faHtml5} />
-                <FontAwesomeIcon className="icon-brands" icon={faSass} />
-                <FontAwesomeIcon className="icon-brands" icon={faBootstrap} />
-              </div>
-              <div className="icon-divider">
-                <FontAwesomeIcon className="icon-brands" icon={faPython} />
-                <FontAwesomeIcon className="icon-brands" icon={faJs} />
-                <FontAwesomeIcon className="icon-brands" icon={faFigma} />
+                <div className="icon-skills-box">
+                  <FontAwesomeIcon className="icon-brands" icon={faReact} />
+                  <div className="icon-skills-box-content">
+                    <p>React</p>
+                  </div>
+                </div>
+                <div className="icon-skills-box">
+                  <FontAwesomeIcon className="icon-brands" icon={faJs} />
+                  <div className="icon-skills-box-content">
+                    <p>JavaScript</p>
+                  </div>
+                </div>
+                <div className="icon-skills-box">
+                  <FontAwesomeIcon className="icon-brands" icon={faHtml5} />
+                  <div className="icon-skills-box-content">
+                    <p>HTML</p>
+                  </div>
+                </div>
+                <div className="icon-skills-box">
+                  <FontAwesomeIcon className="icon-brands" icon={faSass} />
+                  <div className="icon-skills-box-content">
+                    <p>SASS</p>
+                  </div>
+                </div>
+                <div className="icon-skills-box">
+                  <FontAwesomeIcon className="icon-brands" icon={faBootstrap} />
+                  <div className="icon-skills-box-content">
+                    <p>BootStrap</p>
+                  </div>
+                </div>
+                <div className="icon-skills-box">
+                  <FontAwesomeIcon className="icon-brands" icon={faPython} />
+                  <div className="icon-skills-box-content">
+                    <p>Python</p>
+                  </div>
+                </div>
+                <div className="icon-skills-box">
+                  <FontAwesomeIcon className="icon-brands" icon={faFigma} />
+                  <div className="icon-skills-box-content">
+                    <p>Figma</p>
+                  </div>
+                </div>
               </div>
             </div>
             <div className="border-box-bottom-left-skills"></div>
           </div>
           <div className="education-section">
-            <div className="about-header">
-              <div className="btn_content-about">
-                <p>02. education</p>
+            <div className="container-education">
+              <div className="education-header">
+                <div className="btn_content-about">
+                  <p>03. education</p>
+                </div>
+              </div>
+              <div className="flex-education">
+                <p className="triangle-icon">▹</p>
+                <p className="education-text">
+                  Higher Professional Degree Front-end Development at Noroff,
+                  Kristiansand.
+                </p>
+              </div>
+              <div className="flex-education">
+                <p className="triangle-icon">▹</p>
+                <p className="education-text">
+                  Bachelor Degree IT og Informasjonsystemer at USN, Ringeriket
+                </p>
               </div>
             </div>
           </div>
